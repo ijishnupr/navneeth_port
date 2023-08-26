b2eb8c1e72dc035615caa20a9d1a7d07ae64656e
jishnu pr <ijishnupr@gmail.com>
2023-08-26 23:37:27 +0530

    resume added

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,18 @@
 import { Fragment } from "react";
 function About() {
+    const handleDownload = () => {
+        const resumeFilePath = './img/resume.pdf';
 
+    // Create a new anchor element
+    const anchor = document.createElement('a');
+    anchor.href = resumeFilePath;
+    anchor.target = '_blank'; // Open in a new tab
+    anchor.download = 'resume.pdf'; // File name to be downloaded as
+
+    // Simulate a click event to trigger the download
+    anchor.click();
+
+      };
     return <Fragment>
         <div className="container-fluid height">
             <div className="row">
@@ -22,7 +34,7 @@ function About() {
 
 
                         </div>
-                       
+                       <button className="btn btn-outline-light mt-5" onClick={handleDownload} handleDownload>Get resume</button>
                         <img style={{float:"right"}} src='https://camo.githubusercontent.com/97d0c0c4209208d8ec9573c7e213e05872a9f59b703868647b559b77af601cc6/68747470733a2f2f692e70696e696d672e636f6d2f6f726967696e616c732f65382f66342f35332f65386634353334363961336563393765636433353464663436356437333931332e676966' className="aboutimg" />
 
 
