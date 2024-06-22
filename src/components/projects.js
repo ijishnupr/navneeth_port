15b1d90ea07aa10d853b009158d713ec8b7ce263
ijishnupr <ijishnupr@gmail.com>
2024-06-22 12:43:17 +0530

    final without email

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,56 +15,54 @@ function Projects() {
         <div className='row justify-content-center'>
             <Zoom>
 
-            <div className='col service-box' onClick={() => { window.location.href = 'https://curemart.netlify.app/' }}>
-                <div className='proj-head'>
-                CAR REVIEW SYSTEM
-                </div>
-                <div className='proj-body'>
-                    A medical-store management system with token based user authentication and authorization
+                <div className='col service-box' onClick={() => { window.location.href = "" }}>
+                    <div className='proj-head'>
+                        car review system
+                    </div>
+                    <div className='proj-body'>
+                        A vehicle rating portal for comparing top-rated four-wheelers, focusing on electric vehicles, with user-company interactions and profile management.
 
+                    </div>
+                    <div className='proj-foot'>
+                        <p className='badge badge-success'>php</p>
+                        <p className='badge badge-info'>HTML</p>
+                        <p className='badge badge-primary'>css</p>
+                        <p className='badge' style={{ backgroundColor: '#563d7c' }} >Bootstrap</p>
+                    </div>
                 </div>
-                <div className='proj-foot'>
-                    <p className='badge badge-success'>django</p>
-                    <p className='badge badge-info'>react</p>
-                    <p className='badge badge-primary'>css</p>
-                    <p className='badge' style={{ backgroundColor: '#563d7c' }} >Bootstrap</p>
+                <div className='col service-box'>
+                    <div className='proj-head '>
+                        image comparison
+                    </div>
+                    <div className='proj-body'>
+                        Implemented a bank security system with standard cameras capturing photos every second, alerting authorities of anomalies, and reducing storage needs.
+                    </div>
+                    <div className='proj-foot'>
+
+                        <p className='badge badge-secondary'>python</p>
+                    </div>
                 </div>
-            </div>
-            <div className='col service-box'>
-                <div className='proj-head'>
-                    agropedia
+                {/* <div className='col service-box' onClick={() => { window.location.href = 'https://ijishnupr.github.io/sorting-visualizer/' }}>
+                    <div className='proj-head'>sorting-visualizer</div>
+                    <div className='proj-body'>
+                        Visualize th sorting algorithms and performs the operation of algorithms step by step as the algorithms works like Bubble Sort, Merge Sort etc</div>
+                    <div className='proj-foot'>
+                        <p className='badge badge-danger'>HTML</p>
+                        <p className='badge badge-primary'>CSS</p>
+                        <p className=' badge badge-warning'>JS</p>
+                    </div>
                 </div>
-                <div className='proj-body'>
-                    This is an agrcultural management website for farmers
-                </div>
-                <div className='proj-foot'>
-                    <p className='badge badge-danger'>HTML</p>
-                    <p className='badge badge-primary'>css</p>
-                    <p className='badge badge-warning'>JS</p>
-                    <p className='badge badge-secondary'>PHP</p>
-                </div>
-            </div>
-            <div className='col service-box' onClick={() => { window.location.href = 'https://ijishnupr.github.io/sorting-visualizer/' }}>
-                <div className='proj-head'>sorting-visualizer</div>
-                <div className='proj-body'>
-                    Visualize th sorting algorithms and performs the operation of algorithms step by step as the algorithms works like Bubble Sort, Merge Sort etc</div>
-                <div className='proj-foot'>
-                    <p className='badge badge-danger'>HTML</p>
-                    <p className='badge badge-primary'>CSS</p>
-                    <p className=' badge badge-warning'>JS</p>
-                </div>
-            </div>
-            <div className='col service-box' onClick={() => { window.location.href = 'https://highonbuzz.netlify.app/' }}>
-                <div className='proj-head'>High on buzz</div>
-                <div className='proj-body'>
-                    A platform that connects influencers with brands and brands with influencers for collaboration.</div>
-                <div className='proj-foot'>
-                <p className='badge badge-success'>django</p>
-                    <p className='badge badge-info'>react</p>
-                    <p className='badge badge-primary'>css</p>
-                    <p className='badge' style={{ backgroundColor: '#563d7c' }} >Bootstrap</p>
-                </div>
-            </div>
+                <div className='col service-box' onClick={() => { window.location.href = 'https://highonbuzz.netlify.app/' }}>
+                    <div className='proj-head'>High on buzz</div>
+                    <div className='proj-body'>
+                        A platform that connects influencers with brands and brands with influencers for collaboration.</div>
+                    <div className='proj-foot'>
+                        <p className='badge badge-success'>django</p>
+                        <p className='badge badge-info'>react</p>
+                        <p className='badge badge-primary'>css</p>
+                        <p className='badge' style={{ backgroundColor: '#563d7c' }} >Bootstrap</p>
+                    </div>
+                </div> */}
 
             </Zoom>
         </div>
